abde612f3ba27f3e55ba6954a3d7fdd2e65d55a0
Rosinemukandayisenga <rosinemuka2020@gmail.com>
2023-04-27 10:39:25 +0200

    my new portfolio

---
+++ index.js
@@ -0,0 +1,40 @@
+function lineAppear(){
+    document.getElementById("hm").style.textDecoration="underline"
+    document.getElementById("hm").style.textDecorationColor="white"
+    document.getElementById("hm").style.textUnderlineOffset="10px"
+}
+ function lineDisappear(){
+    document.getElementById("hm").style.textDecoration="none"
+ }
+ function lineOn(){
+    document.getElementById("ab").style.textDecoration="underline"
+    document.getElementById("ab").style.textDecorationColor="white"
+    document.getElementById("ab").style.textUnderlineOffset="10px"
+ }
+ function lineOut(){
+    document.getElementById("ab").style.textDecoration="none"
+ }
+ function lineDisplay(){
+    document.getElementById("ser").style.textDecoration="underline"
+    document.getElementById("ser").style.textDecorationColor="white"
+    document.getElementById("ser").style.textUnderlineOffset="10px"
+ }
+ function lineHide(){
+    document.getElementById("ser").style.textDecoration="none"
+ }
+ function line(){
+    document.getElementById("pro").style.textDecoration="underline"
+    document.getElementById("pro").style.textDecorationColor="white"
+    document.getElementById("pro").style.textUnderlineOffset="10px"
+ }
+ function lin(){
+    document.getElementById("pro").style.textDecoration="none"
+ }
+ function on(){
+    document.getElementById("co").style.textDecoration="underline"
+    document.getElementById("co").style.textDecorationColor="white"
+    document.getElementById("co").style.textUnderlineOffset="10px"
+ }
+ function off(){
+    document.getElementById("co").style.textDecoration="none"
+ }
